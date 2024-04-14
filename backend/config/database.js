2d11f69fce6bd98e975bcb79aec0b96633825558
farizzulfiannur <farizzulfiannur@gmail.com>
2024-04-14 23:24:46 +0700

    Get User

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,11 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize('crud_express','root','',{
-    host: 'localhost',
-    dialect: 'mysql'
+dotenv.config();
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
 });
 
 export default db;
--- frontend_react/src/components/UserList.jsx
+++ frontend_react/src/components/UserList.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Navigasi from "./Navigasi";
+import { Container, Table } from "react-bootstrap";
+import axios from 'axios';
 
 function UserList() {
+
+  const [users,setUser] = useState([]);
+
+  useEffect(() => {
+    getUser();
+  });
+
+  const getUser = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      // console.log(response.data);
+      setUser(response.data);
+    } catch (error) {
+      console.log({msg: error})
+    }
+  }
+
   return (
-    <div>UserList</div>
-  )
+    <div>
+      <Navigasi />
+      <Container>
+        <Table className="table-bordered mt-3">
+          <thead>
+            <tr className="table-warning">
+              <th scope="col">No</th>
+              <th scope="col">Nama</th>
+              <th scope="col">Email</th>
+              <th scope="col">Jenis Kelamin</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+            <tr key={user.id}>
+              <th scope="row">{ index+1 }</th>
+              <td>{ user.name }</td>
+              <td>{ user.email }</td>
+              <td>{ user.gender }</td>
+            </tr>
+            ))}
+            {/* <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr> */}
+          </tbody>
+        </Table>
+      </Container>
+    </div>
+  );
 }
 
-export default UserList
+export default UserList;
